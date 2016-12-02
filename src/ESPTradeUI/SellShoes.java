7722bfe2aa31acb57f3b4d1b44de914620a318e4
Zach Germano <zachgermano@gmail.com>
2016-12-02 11:15:33 +0800

    SellShoes

--- src/ESPTradeUI/SellShoes.java
+++ src/ESPTradeUI/SellShoes.java
@@ -6,11 +6,28 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.ws.rs.QueryParam;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
 
 public class SellShoes extends JFrame {
 
@@ -105,7 +122,108 @@ public class SellShoes extends JFrame {
 		textField_4.setColumns(10);
 		
 		JButton btnPostForSelling = new JButton("Post for Selling");
+		btnPostForSelling.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+				interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+				OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+				
+				Gson gson = new GsonBuilder().create();
+				Retrofit retrofit = new Retrofit.Builder()
+						.client(client)
+						.baseUrl("http://localhost:9999/") // a legit base url is needed regardless
+						.addConverterFactory(GsonConverterFactory.create(gson))
+						.build();
+				
+				SellShoesService service = retrofit.create(SellShoesService.class);
+				
+				sendClass sender = new sendClass();
+				sender.setName(textField.getText());
+				sender.setBrand(textField_1.getText());
+				sender.setColor(textField_2.getText());
+				sender.setPrice(Double.parseDouble(textField_3.getText()));
+				sender.setSize(Integer.parseInt(textField_4.getText()));
+				
+				Call<replyClass> call = service.sellShoes(sender.getName(), sender.getBrand(), sender.getColor(), sender.getSize(), sender.getPrice());
+				
+				Response<replyClass> response;
+				
+				try {
+					response = call.execute();
+					System.out.println(response.code());
+					String reply = response.body().getData().toString();
+					System.out.println(reply);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+			}
+		});
 		btnPostForSelling.setBounds(56, 208, 186, 36);
 		contentPane.add(btnPostForSelling);
+	}
+	
+	private interface SellShoesService 
+	{
+		@GET("http://localhost:9999/espTrade/sendMovies") Call<replyClass> sellShoes(@Query("name") String name,
+				@Query("brand") String brand,
+				@Query("color") String color,
+				@Query("size") Integer size,
+				@Query("price") Double price);
+		
+	}
+	
+	public class replyClass
+	{
+		Object data;
+
+		public Object getData() {
+			return data;
+		}
+
+		public void setData(Object data) {
+			this.data = data;
+		}
+	}
+	
+	public class sendClass
+	{
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public Double getPrice() {
+			return price;
+		}
+		public void setPrice(Double price) {
+			this.price = price;
+		}
+		public String getBrand() {
+			return brand;
+		}
+		public void setBrand(String brand) {
+			this.brand = brand;
+		}
+		public String getColor() {
+			return color;
+		}
+		public void setColor(String color) {
+			this.color = color;
+		}
+		public Integer getSize() {
+			return size;
+		}
+		public void setSize(Integer size) {
+			this.size = size;
+		}
+		String name = textField.getText();
+		Double price = Double.parseDouble(textField_3.getText());
+		String brand = textField_1.getText();
+		String color = textField_2.getText();
+		Integer size = Integer.parseInt(textField_4.getText());
 	}
 }
